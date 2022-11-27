2014311374cb267132858e65688cfa3f3af2f61d
Simona Češková <102659100+Shalatik@users.noreply.github.com>
2022-11-27 18:47:36 +0100

    :peeposad:

--- main.cpp
+++ main.cpp
@@ -8,39 +8,18 @@ using namespace std;
 
 int sigmaZ(int distance){
   map<int, int> mapD = {
-      { 5, 0.1 },
-      { 8, 0.2 },
-      { 15, 0.4},
-      { 24, 0.7},
-      { 32, 1},
-      { 50, 2},
-      { 77, 4},
-      { 109, 7},
-      { 135, 10},
-      { 205, 20}
+      {5, 0.1},
+      {8, 0.2},
+      {15, 0.4},
+      {24, 0.7},
+      {32, 1},
+      {50, 2},
+      {77, 4},
+      {109, 7},
+      {135, 10},
+      {205, 20}
   };
 }
-
-
-int concentrationTheorem(int distance)
-{
-  int cars_per_hour = 35000;
-  int average_speed = 80/3.6; // m/s
-  int emissions = emissionCalculate();
-  int q = cars_per_hour/average_speed*emissions;
-  
-  int sigma_z = sigmaZ(distance); 
-  int c = (2*q)/(sqrt(M_PI*2)*sigma_z);
-
-}
-
-//    50      |1|       50
-//            |*|
-//            |*|
-//            |*|
-//            |*|
-//            |*|
-//            |*|
 
 int emissionCalculate()
 {
@@ -55,20 +34,47 @@ int emissionCalculate()
 }
 
 
+int concentration(int distance)
+{
+  int cars_per_hour = 35000;
+  int average_speed = 80/3.6; // m/s
+  int emissions = emissionCalculate();
+  int q = cars_per_hour/average_speed*emissions;
+  
+  int sigma_z = sigmaZ(distance); 
+  int c = (2*q)/(sqrt(M_PI*2)*sigma_z); //*u
+}
+
+
+//    50      |1|       50
+//            |*|
+//            |*|
+//            |*|
+//            |*|
+//            |*|
+//            |*|
+
 int main()
 {
   // Number of rows and columns
-  const char particle = '*';
-  const char clean = ' ';
+  // const char particle = '*';
+  // const char clean = ' ';
   const int rows = 50;
   const int cols = 101;
-  char life[rows][cols];
+  char space[rows][cols];
   
 
+  // i rows
+  for(int i = 0; i < rows; i++){
+    for(int j = 20; j < cols; j++){
+      int x = std::abs((int)(j-50));
+      // cout << x;
+      exit(0);
+      space[i][j] = concentration(x);
+    }
+  }  
 
-  int ans = 0;
-
-
+  // i collumns
 
   return 0;
 }
